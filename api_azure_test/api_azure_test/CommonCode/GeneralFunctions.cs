63712cf27a5e7701ac69934aba583f9191a55a43
ephraiminjamuri-azure <70037244+ephraiminjamuri-azure@users.noreply.github.com>
2020-08-25 11:17:50 -0700

    Update GeneralFunctions.cs

--- api_azure_test/api_azure_test/CommonCode/GeneralFunctions.cs
+++ api_azure_test/api_azure_test/CommonCode/GeneralFunctions.cs
@@ -9,8 +9,7 @@ namespace api_azure_test.Common
     {
         public static string GetConnectionString()
         {
-            return @"Server=tcp:ephraim.database.windows.net,1433;Initial Catalog=ephraim-employee;Persist Security Info=False;User ID=ephraiminjamuri;Password=Sandalwood123$;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-            //return @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Ephraim\Software\SnippetGenerator\SnippetGenerator\eDemoDatabase.mdf;Integrated Security=True";
+            return @"removed connection string due to Microsoft Guidelines";
         }
     }
 }
